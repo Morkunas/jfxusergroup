d0ced3d831351d97835a117c9d6a02571056ee26
Michael Thiele <michael.thiele@saxsys.de>
2012-11-11 16:45:05 +0100

    - added code editor component

---
+++ src/test/java/de/saxsys/presentation/codeeditor/CodeEditorTest.java
@@ -0,0 +1,54 @@
+package de.saxsys.presentation.codeeditor;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javafx.beans.property.Property;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.scene.Node;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CodeEditorTest {
+
+	private CodeEditor codeEditor;
+
+	@Before
+	public void setup() {
+		String sourceCode = "public static void main(String[] args) {\n"
+				+ "\tRectangle r = RectangleBuilder.create().width(#%i).height(#%i).build();\n"
+				+ "}";
+		List<Property<?>> properties = new ArrayList<>();
+		properties.add(new SimpleIntegerProperty(50));
+		properties.add(new SimpleIntegerProperty(100));
+		codeEditor = new CodeEditor();
+		codeEditor.displaySourceCode(sourceCode, properties);
+	}
+
+	@Test
+	public void rightAmountOfLinesAndPropertyFields() {
+		assertEquals(3, codeEditor.getChildrenUnmodifiable().size());
+
+		Node secondLine = codeEditor.getChildrenUnmodifiable().get(1);
+		if (secondLine instanceof HBox) {
+			HBox secondLineHBox = (HBox) secondLine;
+			assertEquals(5, secondLineHBox.getChildrenUnmodifiable().size());
+			
+			assertTrue(secondLineHBox.getChildrenUnmodifiable().get(0) instanceof Text);
+			assertTrue(secondLineHBox.getChildrenUnmodifiable().get(1) instanceof TextField);
+			assertTrue(secondLineHBox.getChildrenUnmodifiable().get(2) instanceof Text);
+			assertTrue(secondLineHBox.getChildrenUnmodifiable().get(3) instanceof TextField);
+			assertTrue(secondLineHBox.getChildrenUnmodifiable().get(4) instanceof Text);
+		} else {
+			fail("The second line has to have property fields that are displyed in an HBox.");
+		}
+	}
+}
