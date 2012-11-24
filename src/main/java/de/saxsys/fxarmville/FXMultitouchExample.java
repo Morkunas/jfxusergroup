33a46bfee731c48e47a71c3d0f76517fcdf4793d
Alexander Casall <alexander.casall@gmail.com>
2012-11-24 16:16:02 +0100

    Multitouch Example with video instead of shapes

--- src/main/java/de/saxsys/fxarmville/FXMultitouchExample.java
+++ src/main/java/de/saxsys/fxarmville/FXMultitouchExample.java
@@ -2,10 +2,13 @@ package de.saxsys.fxarmville;
 
 import javafx.animation.FadeTransition;
 import javafx.animation.FadeTransitionBuilder;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Point3D;
 import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.control.Label;
@@ -14,22 +17,27 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.input.RotateEvent;
 import javafx.scene.input.SwipeEvent;
 import javafx.scene.input.TouchEvent;
-import javafx.scene.input.TouchPoint;
 import javafx.scene.input.ZoomEvent;
 import javafx.scene.layout.Pane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
-import javafx.scene.shape.Rectangle;
 import javafx.util.Duration;
 
 public class FXMultitouchExample extends Pane {
 
+	final MediaExample media = new MediaExample();
+
 	public FXMultitouchExample() {
+		getChildren().add(media);
+		media.setTranslateX(50);
+		media.setTranslateY(50);
 		createEventHandler();
 	}
 
 	private void createEventHandler() {
-		initMouseHandler();
 
 		initTouch();
 
@@ -40,55 +48,33 @@ public class FXMultitouchExample extends Pane {
 		initRotation();
 	}
 
-	private void initMouseHandler() {
-		this.setOnMouseClicked(new EventHandler<MouseEvent>() {
-			@Override
-			public void handle(MouseEvent event) {
-				getChildren().clear();
-				addCircleForMouseEvent(event);
-				DemoLabel label = new DemoLabel("Mouse click");
-				label.setTranslateX(event.getX());
-				label.setTranslateY(event.getY());
-				getChildren().add(label);
-			}
-		});
-		this.setOnMouseDragged(new EventHandler<MouseEvent>() {
-			@Override
-			public void handle(MouseEvent event) {
-				getChildren().clear();
-				addCircleForMouseEvent(event);
-				DemoLabel label = new DemoLabel("Mouse dragged", event.getX(),
-						event.getX());
-				getChildren().add(label);
-			}
-		});
-	}
-
 	private void initTouch() {
-		this.setOnTouchStationary(new EventHandler<TouchEvent>() {
+
+		final BooleanProperty touchStat = new SimpleBooleanProperty();
+		media.setOnTouchStationary(new EventHandler<TouchEvent>() {
 			@Override
 			public void handle(TouchEvent touchEvent) {
-				getChildren().clear();
-				for (TouchPoint touch : touchEvent.getTouchPoints()) {
-					createCircleForTouch(touchEvent);
-					DemoLabel label = new DemoLabel("Stationary Touch", touch
-							.getX(), touch.getY());
-					getChildren().add(label);
+				touchStat.set(true);
+				media.setEffect(new DropShadow());
+			}
+		});
+		media.setOnTouchMoved(new EventHandler<TouchEvent>() {
+			@Override
+			public void handle(TouchEvent touchEvent) {
+				if (touchStat.get()) {
+					media.setTranslateX(touchEvent.getTouchPoint().getX()
+							- media.getBoundsInLocal().getWidth() / 2);
+					media.setTranslateY(touchEvent.getTouchPoint().getY()
+							- media.getBoundsInLocal().getHeight() / 2);
 				}
 			}
 		});
-		this.setOnTouchMoved(new EventHandler<TouchEvent>() {
+		media.setOnTouchReleased(new EventHandler<TouchEvent>() {
+
 			@Override
-			public void handle(TouchEvent touchEvent) {
-				getChildren().clear();
-				for (TouchPoint touch : touchEvent.getTouchPoints()) {
-					createCircleForTouch(touchEvent);
-					DemoLabel label = new DemoLabel("Touch moved",
-							touch.getX(), touch.getY());
-					label.setTranslateX(touch.getX());
-					label.setTranslateY(touch.getY());
-					getChildren().add(label);
-				}
+			public void handle(TouchEvent arg0) {
+				touchStat.set(false);
+				media.setEffect(null);
 			}
 		});
 	}
@@ -97,7 +83,6 @@ public class FXMultitouchExample extends Pane {
 		this.setOnSwipeDown(new EventHandler<SwipeEvent>() {
 			@Override
 			public void handle(SwipeEvent event) {
-				getChildren().clear();
 				DemoLabel label = new DemoLabel("Swipe down", event.getX(),
 						event.getY());
 				getChildren().add(label);
@@ -107,7 +92,6 @@ public class FXMultitouchExample extends Pane {
 		this.setOnSwipeUp(new EventHandler<SwipeEvent>() {
 			@Override
 			public void handle(SwipeEvent event) {
-				getChildren().clear();
 				DemoLabel label = new DemoLabel("Swipe up", event.getX(), event
 						.getY());
 				getChildren().add(label);
@@ -117,7 +101,6 @@ public class FXMultitouchExample extends Pane {
 		this.setOnSwipeRight(new EventHandler<SwipeEvent>() {
 			@Override
 			public void handle(SwipeEvent event) {
-				getChildren().clear();
 				DemoLabel label = new DemoLabel("Swipe right", event.getX(),
 						event.getY());
 				getChildren().add(label);
@@ -127,7 +110,7 @@ public class FXMultitouchExample extends Pane {
 		this.setOnSwipeLeft(new EventHandler<SwipeEvent>() {
 			@Override
 			public void handle(SwipeEvent event) {
-				getChildren().clear();
+
 				DemoLabel label = new DemoLabel("Swipe left", event.getX(),
 						event.getY());
 				getChildren().add(label);
@@ -136,78 +119,30 @@ public class FXMultitouchExample extends Pane {
 	}
 
 	private void initZoom() {
-		final Circle circle = new Circle(50);
-
-		this.setOnZoomStarted(new EventHandler<ZoomEvent>() {
+		media.setOnZoom(new EventHandler<ZoomEvent>() {
 			@Override
 			public void handle(ZoomEvent event) {
-				DemoLabel label = new DemoLabel("Zoom detected", event.getX(),
-						event.getY());
-				circle.setFill(Color.ORANGE);
-				circle.setCenterX(event.getX());
-				circle.setCenterY(event.getY());
-				getChildren().addAll(circle, label);
-			}
-		});
-
-		this.setOnZoom(new EventHandler<ZoomEvent>() {
-			@Override
-			public void handle(ZoomEvent event) {
-				circle.setScaleX(circle.getScaleX() * event.getZoomFactor());
-				circle.setScaleY(circle.getScaleY() * event.getZoomFactor());
-				circle.setCenterX(event.getX());
-				circle.setCenterY(event.getY());
-
-			}
-		});
-		this.setOnZoomFinished(new EventHandler<ZoomEvent>() {
-			@Override
-			public void handle(ZoomEvent event) {
-				circle.setScaleX(1);
-				circle.setScaleY(1);
-				getChildren().clear();
+				// Todo pivotpunkt?
+				media.setScaleX(media.getScaleX() * event.getZoomFactor());
+				media.setScaleY(media.getScaleY() * event.getZoomFactor());
 			}
 		});
 	}
 
 	private void initRotation() {
-		getChildren().clear();
-		final DemoLabel label = new DemoLabel("Rotate");
-		final Rectangle rect = new Rectangle(80, 80);
-		rect.setFill(Color.RED);
-
-		this.setOnRotationStarted(new EventHandler<RotateEvent>() {
+		media.setRotationAxis(new Point3D(
+				media.getBoundsInLocal().getWidth() / 2, media
+						.getBoundsInLocal().getHeight() / 2, 0));
+		media.setOnRotate(new EventHandler<RotateEvent>() {
 			@Override
 			public void handle(RotateEvent event) {
-				getChildren().addAll(rect, label);
-				label.setTranslateX(event.getX());
-				label.setTranslateY(event.getY());
-				rect.setX(event.getX() - rect.getWidth() / 2);
-				rect.setY(event.getY() - rect.getHeight() / 2);
-			}
-		});
-
-		this.setOnRotate(new EventHandler<RotateEvent>() {
-			@Override
-			public void handle(RotateEvent event) {
-				rect.setRotate(rect.getRotate() + event.getAngle());
-			}
-		});
-
-		this.setOnRotationFinished(new EventHandler<RotateEvent>() {
-			@Override
-			public void handle(RotateEvent event) {
-				getChildren().clear();
+				media.setRotate(media.getRotate() + event.getAngle());
 			}
 		});
 	}
 
 	private class DemoLabel extends Group {
 		Label label = new Label();
-
-		public DemoLabel(String text) {
-			this(text, 0, 0);
-		}
 
 		public DemoLabel(String text, double x, double y) {
 			setTranslateX(x);
@@ -245,11 +180,23 @@ public class FXMultitouchExample extends Pane {
 		getChildren().add(circle);
 	}
 
-	private void createCircleForTouch(TouchEvent event) {
-		Circle circle = new Circle(50);
-		circle.setFill(Color.ORANGE);
-		circle.setCenterX(event.getTouchPoint().getX());
-		circle.setCenterY(event.getTouchPoint().getY());
-		getChildren().add(circle);
+	private class MediaExample extends Pane {
+		// TODO auf platte laden
+		private MediaPlayer mediaPlayer;
+		final double mediaWidth = 240;
+		final double mediaHeight = 145;
+
+		public MediaExample() {
+			mediaPlayer = new MediaPlayer(new Media(ClassLoader
+					.getSystemResource(
+							"JavaRap_ProRes_H264_768kbit_Widescreen.mp4")
+					.toString()));
+			mediaPlayer.setAutoPlay(true);
+			MediaView playerPane = new MediaView(mediaPlayer);
+			this.setMinSize(mediaWidth, mediaHeight);
+			this.setPrefSize(mediaWidth, mediaHeight);
+			this.setMaxSize(mediaWidth, mediaHeight);
+			this.getChildren().add(playerPane);
+		}
 	}
 }
--- src/main/java/de/saxsys/fxarmville/Main.java
+++ src/main/java/de/saxsys/fxarmville/Main.java
@@ -1,7 +1,6 @@
 package de.saxsys.fxarmville;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
@@ -18,9 +17,9 @@ public class Main extends Application {
 	// **** CODE FÜR FXML später hinzufügen ****
 	@Override
 	public void start(Stage stage) throws Exception {
-		Pane rootPane = (Pane) FXMLLoader.load(Main.class
-				.getResource("/root.fxml"));
-		// Pane rootPane = new FXMultitouchExample();
+		// Pane rootPane = (Pane) FXMLLoader.load(Main.class
+		// .getResource("/root.fxml"));
+		Pane rootPane = new FXMultitouchExample();
 
 		Scene rootScene = new Scene(rootPane, 1000, 500);
 
