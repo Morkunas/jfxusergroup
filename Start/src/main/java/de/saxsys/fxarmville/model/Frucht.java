e6a5e0ce6da269028d6a334493a856b319902b1c
Michael Thiele <michael.thiele@saxsys.de>
2013-01-10 17:28:21 +0100

    renaming

--- Start/src/main/java/de/saxsys/fxarmville/model/Frucht.java
+++ Start/src/main/java/de/saxsys/fxarmville/model/Frucht.java
@@ -1,6 +1,11 @@
 package de.saxsys.fxarmville.model;
 
+import java.util.Random;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
+import javafx.animation.TimelineBuilder;
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.ReadOnlyBooleanProperty;
@@ -8,6 +13,7 @@ import javafx.beans.property.ReadOnlyDoubleProperty;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.scene.image.Image;
+import javafx.util.Duration;
 import de.saxsys.fxarmville.model.util.FruchtBildLader;
 import de.saxsys.fxarmville.presentation.special.BugTracker;
 
@@ -22,10 +28,10 @@ public class Frucht {
 	/*
 	 * Util: Lebensabschnitte
 	 */
-	private final BooleanProperty istReifProperty = new SimpleBooleanProperty();
-	private final BooleanProperty istFauligProperty = new SimpleBooleanProperty();
-	private final BooleanProperty istEingegangenProperty = new SimpleBooleanProperty();
-	private final BooleanProperty istGeerntetWordenProperty = new SimpleBooleanProperty();
+	private final BooleanProperty istReif = new SimpleBooleanProperty();
+	private final BooleanProperty istFaulig = new SimpleBooleanProperty();
+	private final BooleanProperty istEingegangen = new SimpleBooleanProperty();
+	private final BooleanProperty istGeerntetWorden = new SimpleBooleanProperty();
 
 	private final LebensZyklus lebensZyklus = new LebensZyklus();
 	private final String bildName;
@@ -48,7 +54,7 @@ public class Frucht {
 
 	public void ernten() {
 		lebensZyklus.ernten();
-		istGeerntetWordenProperty.set(true);
+		istGeerntetWorden.set(true);
 	}
 
 	/*
@@ -78,21 +84,21 @@ public class Frucht {
 	 */
 
 	public ReadOnlyBooleanProperty istReifProperty() {
-		return istReifProperty;
+		return istReif;
 	}
 
 	public ReadOnlyBooleanProperty istFauligProperty() {
-		return istFauligProperty;
+		return istFaulig;
 	}
 
 	public ReadOnlyBooleanProperty istEingegangenProperty() {
-		return istEingegangenProperty;
+		return istEingegangen;
 	}
 
 	public ReadOnlyBooleanProperty istGeerntetWordenProperty() {
-		return istGeerntetWordenProperty;
+		return istGeerntetWorden;
 	}
-	
+
 	private double getReifedauer() {
 		return getLebenszeit() / 16d;
 	}
